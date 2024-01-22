954dfef7bfe8a380168073b1afa554b8751df3ff
Ilan Velter <ilan.velter@etu.umontpellier.fr>
2024-01-22 22:03:45 +0100

    add: animations du player importées, il reste à les lui appliquer

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -1,13 +1,15 @@
 extends CharacterBody2D
 
-@onready var _animation_player = $AnimationPlayer
+@onready var _animation_player = $AnimatedSprite2D
 const SPEED = 20000
 var direction = "up" 
 
 func update_direction():
 	if Input.is_action_pressed("move_left"):
+		_animation_player.flip_h = false
 		direction = "left"
 	if Input.is_action_pressed("move_right"):
+		_animation_player.flip_h = true
 		direction = "right"
 	if Input.is_action_pressed("move_up"):
 		direction = "up"
@@ -16,11 +18,11 @@ func update_direction():
 
 #Renvoie vrai si le joueur a utilisé son épée à cette frame là
 func swung_sword() -> bool:
-	return Input.is_action_pressed("swing_sword")
+	return Input.is_action_just_pressed("swing_sword")
 
 #Renvoie vrai si le joueur a interagi avec un objet
 func interacted() -> bool:
-	return Input.is_action_pressed("interact")
+	return Input.is_action_just_pressed("interact")
 
 func _physics_process(delta):
 	# Mouvement du joueur
@@ -32,19 +34,28 @@ func _physics_process(delta):
 	# Animation
 	update_direction()
 	if Input.is_anything_pressed():
+		# print_debug("oui") # ça print il faut fix le reste imo
 		if swung_sword():
-			pass
+			match (direction):
+				"up":
+					_animation_player.play("up_attack")
+				"down":
+					_animation_player.play("down_attack")
+				"left":
+					_animation_player.play("left_attack")
+				"right":
+					_animation_player.play("left_attack")
 		elif interacted():
-			pass
+			_animation_player.play("left_pick_up")
 		else:
 			pass
 	else:
 		match (direction):
 			"up":
-				pass
+				_animation_player.play("up_idle")
 			"down":
 				_animation_player.play("down_idle")
 			"left":
-				pass
+				_animation_player.play("left_idle")
 			"right":
-				pass
+				_animation_player.play("left_idle")
