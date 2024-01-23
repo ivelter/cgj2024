d9f9b47a3f6f864f0097a596b58a6fc9fb528187
Ilan Velter <ilan.velter@etu.umontpellier.fr>
2024-01-23 19:52:04 +0100

    ajout des autres boites de dialogue au spawn

---
+++ scripts/entities/Grave2.gd
@@ -0,0 +1,23 @@
+extends CharacterBody2D
+
+@onready var collider = $Area2D
+var isEntered = false 
+var isOpened = false
+
+func _ready():
+	collider.connect("body_entered", on_grave_entered)
+	collider.connect("body_exited", on_grave_exited)
+
+func _physics_process(delta):
+	if (Input.is_action_just_pressed("interact") and isEntered):
+		DialogueBox.display_stone2()
+		isOpened = true 
+	if (Input.is_action_just_pressed("swing_sword") and isOpened):
+		DialogueBox.closeBox()
+		isOpened = false
+
+func on_grave_entered(body: Node):
+	isEntered = true
+
+func on_grave_exited(body: Node):
+	isEntered = false
