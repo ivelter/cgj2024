a1beb712e0fdcf2e78d26483e40f40e3477060d4
Ilan Velter <ilan.velter@etu.umontpellier.fr>
2024-01-23 22:27:46 +0100

    import sound effects + ils se jouent correctement

--- scripts/AudioManager.gd
+++ scripts/AudioManager.gd
@@ -6,6 +6,25 @@ var bus = "master"
 var available = []  # The available players.
 var queue = []  # The queue of sounds to play.
 
+var soundPath = "res://assets/audio/sounds/"
+var dieSound = "die/die.mp3"
+var hurtSounds = [
+	"hurt/hurt2.mp3",
+	"hurt/hurt3.mp3"
+]
+var pickSounds = [
+	"pick/pick1.mp3",
+	"pick/pick2.mp3",
+	"pick/pick3.mp3",
+	"pick/pick4.mp3"
+]
+var swordSounds = [
+	"sword_hit1.mp3",
+	"sword_hit2.mp3",
+	"sword_void1.mp3",
+	"sword_void2.mp3"
+]
+
 func _ready():
 	# Create the pool of AudioStreamPlayer nodes.
 	for i in num_players:
@@ -21,6 +40,18 @@ func _on_stream_finished(stream):
 
 func play(sound_path):
 	queue.append(sound_path)
+
+func playDieSound():
+	play(soundPath + dieSound)
+
+func playHurtSound():
+	play(soundPath + hurtSounds[randi() % hurtSounds.size()])
+
+func playPickSound():
+	play(soundPath + pickSounds[randi() % pickSounds.size()])
+
+func playSwordSound():
+	play(soundPath + swordSounds[randi() % swordSounds.size()])
 
 func _process(delta):
 	# Play a queued sound if any players are available.
