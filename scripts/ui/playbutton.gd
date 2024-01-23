4c1ddb7d6a41f2943c511ad6b7651343e4d214bb
Ilan Velter <ilan.velter@etu.umontpellier.fr>
2024-01-23 11:04:59 +0100

    généralisation player en entity + import du player dans le level1

--- scripts/ui/playbutton.gd
+++ scripts/ui/playbutton.gd
@@ -13,4 +13,4 @@ func _process(delta):
 # Appelée quand on clique sur le bouton
 func _on_pressed():
 	# print_debug("Clic!")
-	get_tree().change_scene_to_file("res://scenes/game/TestSceneBri.tscn")
+	get_tree().change_scene_to_file("res://scenes/game/Level1.tscn")
