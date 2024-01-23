21160efc054d7b5d84e2e04ac62d8ccd15ef8f31
Ilan Velter <ilan.velter@etu.umontpellier.fr>
2024-01-23 13:48:21 +0100

    ajout logo

--- entities/enemies/skeleton/skeleton.gd
+++ entities/enemies/skeleton/skeleton.gd
@@ -1,10 +1,11 @@
 extends CharacterBody2D
 
 @onready var _animated_sprite = $AnimatedSprite2D
-@onready var target = get_node("ress://entities/player.player.tscn").getPosition()
+@onready var target = $Player
 var speed = 500
 
 func _process(_delta):
+	position = target.getPosition()
 	if velocity.x == 0 && velocity.y == 0:
 		_animated_sprite.play("idling")
 	else:
