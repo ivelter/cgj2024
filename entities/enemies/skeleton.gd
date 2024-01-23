a049c6133c9193b1a1411e6e3af7e48b300cee00
chabauda <antonin.chabaud-pech@etu.umontpellier.fr>
2024-01-23 10:33:58 +0100

    skeleton

---
+++ entities/enemies/skeleton.gd
@@ -0,0 +1,20 @@
+extends CharacterBody2D
+
+@onready var _animated_sprite = $AnimatedSprite2D
+
+var speed = 500
+var target = position
+
+func _process(_delta):
+	if velocity.x == 0 && velocity.y == 0:
+		_animated_sprite.play("idling")
+	else:
+		_animated_sprite.play("runing")
+		
+	# target = get_node("CollisionShape2D").getPosition()
+		
+	velocity = position.direction_to(target) * speed
+	
+	# look_at(target)
+	if position.distance_to(target) > 10:
+		move_and_slide()
