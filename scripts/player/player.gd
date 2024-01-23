7e7830c2765f837bda0ced854a91e29f5b5a38ae
Ilan Velter <ilan.velter@etu.umontpellier.fr>
2024-01-23 09:32:13 +0100

    update: animations, il reste actions à fix

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -2,19 +2,19 @@ extends CharacterBody2D
 
 @onready var _animation_player = $AnimatedSprite2D
 const SPEED = 20000
-var direction = "up" 
+var directionOfPlayer = "up" 
 
 func update_direction():
 	if Input.is_action_pressed("move_left"):
 		_animation_player.flip_h = false
-		direction = "left"
+		directionOfPlayer = "left"
 	if Input.is_action_pressed("move_right"):
 		_animation_player.flip_h = true
-		direction = "right"
+		directionOfPlayer = "right"
 	if Input.is_action_pressed("move_up"):
-		direction = "up"
+		directionOfPlayer = "up"
 	if Input.is_action_pressed("move_down"):
-		direction = "down"
+		directionOfPlayer = "down"
 
 #Renvoie vrai si le joueur a utilisé son épée à cette frame là
 func swung_sword() -> bool:
@@ -24,19 +24,11 @@ func swung_sword() -> bool:
 func interacted() -> bool:
 	return Input.is_action_just_pressed("interact")
 
-func _physics_process(delta):
-	# Mouvement du joueur
-	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
-	velocity = direction * SPEED * delta
-
-	move_and_slide()
-	
-	# Animation
-	update_direction()
+#Update l'animation en fonction de si le joueur a fait une action
+func animationInteractionUpdate() -> void:
 	if Input.is_anything_pressed():
-		# print_debug("oui") # ça print il faut fix le reste imo
 		if swung_sword():
-			match (direction):
+			match (directionOfPlayer):
 				"up":
 					_animation_player.play("up_attack")
 				"down":
@@ -48,9 +40,37 @@ func _physics_process(delta):
 		elif interacted():
 			_animation_player.play("left_pick_up")
 		else:
-			pass
+			match (directionOfPlayer):
+				"up":
+					_animation_player.play("up_walk")
+				"down":
+					_animation_player.play("down_walk")
+				"left":
+					_animation_player.play("left_walk")
+				"right":
+					_animation_player.play("left_walk")
+
+func _ready():
+	_animation_player.play(directionOfPlayer + "_idle")
+
+func _physics_process(delta):
+	# Mouvement du joueur
+	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	velocity = direction * SPEED * delta
+
+	move_and_slide()
+	
+	# Animation
+	update_direction()
+	# Override de l'animation si jamais le joueur fait une action
+	animationInteractionUpdate()
+
+# Signal handler for animation finished
+func _on_animation_player_animation_finished(anim_name: String) -> void:
+	if Input.is_anything_pressed():
+		animationInteractionUpdate()
 	else:
-		match (direction):
+		match (directionOfPlayer):
 			"up":
 				_animation_player.play("up_idle")
 			"down":
